32eb905555ae3edbf88cfaefdc0c9ee514e54b82
Kevin Liu <kevinxo328@gmail.com>
2025-07-29 11:09:31 +0800

    Refactor: updates configuration for output directory

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,6 +1,7 @@
-import { defineConfig } from 'wxt';
+import {defineConfig} from "wxt";
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
-  modules: ['@wxt-dev/module-vue'],
+  modules: ["@wxt-dev/module-vue"],
+  outDir: "dist",
 });
